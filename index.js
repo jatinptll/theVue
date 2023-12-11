c3102bc821dffcbc81f5d8651543c6ebeeef7fd3
jatinptll <justforloginsignup@gmail.com>
2023-12-12 02:57:32 +0530

    added background animation to about us page and added index.js for background journey. Here we go again!!!

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById('postbutton').addEventListener('click', function() {
+//             // Get the text entered in the textarea
+//             var textareaValue = document.querySelector('.textarea-home').value;
+
+//             // Create a new card element
+//             var card = document.createElement('div');
+//             card.className = 'card1';
+//             card.textContent = textareaValue;
+
+//             // Append the card to the body (you can adjust this based on your structure)
+//             document.body.appendChild(card);
+//         });
